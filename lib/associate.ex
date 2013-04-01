6ef34b4a0235718888bfe64ef7e7c6cfb0f0eb38
Jeremy Huffman <jeremy@jeremyhuffman.com>
2013-03-31 20:23:26 -0400

    impl and tests HashDict, :dict, :orddict, tuples

--- lib/associate.ex
+++ lib/associate.ex
@@ -1,10 +1,29 @@
 defprotocol Nested.Associate do
+  @only [Record, Tuple, List]
   def assoc(structure, index, value)
   def get(structure, index)
   #def update(structure, index, value)
 end
 
+defimpl Nested.Associate, for: HashDict do
+  def assoc(dict, key, value) do
+    Dict.put(dict, key, value)
+  end
+
+  def get(dict, key) do
+    Dict.get(dict,key)
+  end
+end
+
+
 defimpl Nested.Associate, for: Tuple do
+  
+  #for :dicts
+  def assoc(dict, key, value) 
+    when is_record(dict) and elem(dict, 0) == :dict  do
+    :dict.store(key, value, dict)
+  end
+  
   # for Records
   def assoc(record, attribute, value) when is_record(record) do
     module = elem(record,0)
@@ -14,6 +33,12 @@ defimpl Nested.Associate, for: Tuple do
   # for arbitrary tuples
   def assoc(tuple, index, value) when is_integer(index) do
     setelem(tuple, index, value)
+  end
+
+  # for :dicts 
+  def get(dict, key)
+    when is_record(dict) and elem(dict, 0) == :dict  do
+    :dict.fetch(key, dict)
   end
 
   # for Records
@@ -26,14 +51,16 @@ defimpl Nested.Associate, for: Tuple do
   def get(tuple, index) when is_integer(index) do
     elem(tuple,index)
   end
+
 end
 
 defimpl Nested.Associate, for: List do
-  # for Keywords list
+  # for Keywords or orddict
   def assoc(words, key, value) when is_atom(key) do
     Keyword.put(words, key, value)
   end
 
+  # for Keywords or orddict
   def get(words, key) when is_atom(key) do
     Keyword.get(words,key)
   end
--- test/nested_test.exs
+++ test/nested_test.exs
@@ -5,7 +5,7 @@ defmodule NestedTest do
   import Nested
 
   defrecord Address, street: nil, city: nil, state: nil
-  defrecord Person, first_name: nil, last_name: nil, address: nil
+  defrecord Person, first_name: nil, last_name: nil, address: nil, phone_numbers: nil
 
   setup do
     {:ok, 
@@ -24,6 +24,54 @@ defmodule NestedTest do
     more_words = [better: "best", worse: "worst"]
     words = assoc_in(setup[:words], [:inner], more_words)
     assert words[:inner][:better] == "best" 
+  end
+
+  test "assoc-in records and keywords", setup do
+    work = setup[:home].city "Barter Town"
+    jeremy = assoc_in(setup[:jeremy], [:address], [home: setup[:home], work: work])
+    jeremy = assoc_in(jeremy, [:address, :work, :state], "AU")
+    jeremy = assoc_in(jeremy, [:address, :home], setup[:home].state "SC")
+    assert jeremy.address[:work].state == "AU"
+    assert jeremy.address[:work].city == "Barter Town"
+    assert jeremy.address[:home].state == "SC"
+  end
+
+  test "assoc-in ordinal tuples, records and keywords", setup do
+    work = setup[:home].city "Barter Town"
+    jeremy = {assoc_in(setup[:jeremy], [:address], [home: setup[:home], work: work]),"frogs"}
+    jeremy = assoc_in(jeremy, [0, :address, :work, :state], "AU")
+    jeremy = assoc_in(jeremy, [0, :address, :home], setup[:home].state "SC")
+    assert elem(jeremy,0).address[:work].state == "AU"
+    assert elem(jeremy,0).address[:work].city == "Barter Town"
+    assert elem(jeremy,0).address[:home].state == "SC"
+  end
+
+  test "assoc-in orddicts and records", setup do
+    numbers = Dict.put(:orddict.new, :home, "867-5309")
+    numbers = Dict.put(numbers, :work, "555-2121")
+    jeremy = setup[:jeremy].phone_numbers(numbers)
+    people = Dict.put(:orddict.new, :jeremy, jeremy)
+    people = assoc_in(people, [:jeremy, :phone_numbers, :work], "555-9191")
+    assert people[:jeremy].phone_numbers[:work] == "555-9191"
+  end
+
+  test "assoc-in HashDicts and records", setup do
+    numbers = Dict.put(HashDict.new, :home, "867-5309")
+    numbers = Dict.put(numbers, :work, "555-2121")
+    jeremy = setup[:jeremy].phone_numbers(numbers)
+    people = Dict.put(HashDict.new, :jeremy, jeremy)
+    people = assoc_in(people, [:jeremy, :phone_numbers, :work], "555-9191")
+    assert people[:jeremy].phone_numbers[:work] == "555-9191"
+  end
+
+  test "assoc-in :dict and records", setup do
+    numbers = :dict.store(:home, "867-5309", :dict.new)
+    numbers = :dict.store(:work, "555-2121", numbers)
+    jeremy = setup[:jeremy].phone_numbers(numbers)
+    people = :dict.store(:jeremy, jeremy, :dict.new)
+    people = assoc_in(people, [:jeremy, :phone_numbers, :work], "555-9191")
+    people = :dict.fetch(:jeremy, people)
+    assert :dict.fetch(:work, people.phone_numbers) == "555-9191"
   end
 
 end
