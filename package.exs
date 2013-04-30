4d44f7f8f53868048398c8cfa4412168d796d1fe
Jeremy Huffman <jeremy@jeremyhuffman.com>
2013-04-29 22:23:10 -0400

    add expm package defintion

---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "nested", description: "functions for manipulating hierarchies of structures in Elixir",
+                 version: "0.1.0", keywords: ["nested", "put_in", "assoc-in"], 
+                 maintainers: [[name: "Jeremy Huffman", 
+                                email: "jeremy@jeremyhuffman.com"]],
+                 repositories: [[github: "jeremyjh/nested"]])
